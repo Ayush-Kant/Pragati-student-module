b2801b0205aa9faf05b0bcb4e1dd865a032a7184
Ankit Kumar Suman <sumanankitkumar033@gmail.com>
2026-05-24 00:51:11 +0530

    Implemented student notifications API

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import connectDB from "./config/db.js";
 import mentorRoutes from "./routes/mentor.routes.js";
 import adminDashboardRoutes from "./routes/admin.dashboard.routes.js";
 import contentRoutes from "./routes/content.routes.js";
+import notificationRoutes from "./routes/notification.routes.js";
 
 const PORT = process.env.PORT || 5001;
 
@@ -15,6 +16,8 @@ app.use("/api/mentor", mentorRoutes);
 app.use("/api/v1/admin/dashboard", adminDashboardRoutes);
 
 app.use("/api/mentor", contentRoutes);
+
+app.use("/api/student/notifications", notificationRoutes);
 
 connectDB(process.env.POSTGRESQL_URI).then(() => {
   app.listen(PORT, () => {
