9c3fed22faadc2e26135677c36736a955798fb36
gurramjyothirmayi <gurramjyothirmayi8@gmail.com>
2026-05-27 13:29:07 +0530

    Added assessments and offers pages

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -4,31 +4,76 @@ import MainLayout from "../layout/MainLayout";
 
 import CompanyDashboard from "../features/company/pages/CompanyDashboard";
 
+/* Existing Pages */
 import Drives from "../pages/Drives";
 import Candidates from "../pages/Candidates";
-import Assessments from "../pages/Assessments";
 import Interviews from "../pages/Interviews";
 import Training from "../pages/Training";
 import Messages from "../pages/Messages";
-import Offers from "../pages/Offers";
+import Offers from "../features/company/offers/Offers";
 import Reports from "../pages/Reports";
 import Settings from "../pages/Settings";
+
+/* New Assessments Feature */
+import Assessments from "../features/company/assessments/Assessments";
 
 const AppRoutes = () => {
   return (
     <Routes>
       <Route element={<MainLayout />}>
-        <Route path="/" element={<CompanyDashboard />} />
 
-        <Route path="/drives" element={<Drives />} />
-        <Route path="/candidates" element={<Candidates />} />
-        <Route path="/assessments" element={<Assessments />} />
-        <Route path="/interviews" element={<Interviews />} />
-        <Route path="/training" element={<Training />} />
-        <Route path="/messages" element={<Messages />} />
-        <Route path="/offers" element={<Offers />} />
-        <Route path="/reports" element={<Reports />} />
-        <Route path="/settings" element={<Settings />} />
+        {/* Dashboard */}
+        <Route
+          path="/"
+          element={<CompanyDashboard />}
+        />
+
+        {/* Sidebar Pages */}
+        <Route
+          path="/drives"
+          element={<Drives />}
+        />
+
+        <Route
+          path="/candidates"
+          element={<Candidates />}
+        />
+
+        <Route
+          path="/assessments"
+          element={<Assessments />}
+        />
+
+        <Route
+          path="/interviews"
+          element={<Interviews />}
+        />
+
+        <Route
+          path="/training"
+          element={<Training />}
+        />
+
+        <Route
+          path="/messages"
+          element={<Messages />}
+        />
+
+        <Route
+          path="/offers"
+          element={<Offers />}
+        />
+
+        <Route
+          path="/reports"
+          element={<Reports />}
+        />
+
+        <Route
+          path="/settings"
+          element={<Settings />}
+        />
+
       </Route>
     </Routes>
   );
