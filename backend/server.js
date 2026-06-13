3dab88e09141ee28df2691e5238f5a4c5772ef09
Pranjal Singh <ppranjalsingh13@gmail.com>
2026-06-13 18:39:44 +0530

    Revert "feat: Add Interview Management Module (Module 5)"

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,5 @@
 import express from "express";
 import connectDB from "./config/db.js";
-import mentorRoutes from "./routes/mentor.routes.js";
-import interviewRoutes from "./routes/interview.routes.js";
-
 
 const PORT = process.env.PORT || 5000;
 
@@ -10,16 +7,11 @@ const app = express();
 
 app.use(express.json());
 
-
 app.get("/", (req, res) => {
     res.json({
         message: "Backend is running",
     });
 });
-
-app.use("/api/mentor", mentorRoutes);
-app.use("/api/v1/company/interviews", interviewRoutes);
-
 
 app.listen(PORT, () => {
     console.log(`✅ Server running on PORT : ${PORT}`);
