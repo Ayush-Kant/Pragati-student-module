4b53dd5f40a03e8ef45ca85e54639e77d7e50b6c
Himanshu Paswan <himanshuspaswan2004@gmail.com>
2026-06-16 19:10:22 +0530

    fix: update App routing and set login as default route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,15 @@
 
+
 import { Toaster } from "react-hot-toast";
 import { Routes, Route, Navigate } from 'react-router-dom';
+
 
 // ── Auth Pages  ──
 import LoginPage from "./features/auth/LoginPage";
 import RegisterPage from "./features/auth/RegisterPage";
-import EditProfileForm from "./features/college/profile/components/edit-profile/EditProfileForm";
-import DashboardLayout from "./features/college/dashboard/components/layout/DashboardLayout";
-import StatsGrid from "./features/college/dashboard/components/stats/StatsGrid";
+
+
+
 
 // ── Student Module ───────────────────────────────────────────────────────────
 import { AuthProvider} from './context/AuthContext';
@@ -19,53 +21,58 @@ import collegeRoute from "./features/college/routes/AppRoutes";
 import NotFoundPage from "./routes/NotFoundPage";
 import CompanyRoute from "./features/company/routes/CompanyRoute";
 
+
 function App() {
   return (
     <AuthProvider>
       <Toaster />
       <Routes>
-        
-        <Route
-  path="/"
-  element={
-    <DashboardLayout>
-      <div className="p-6">
-        <StatsGrid/> </div>
-    </DashboardLayout>
-  }
-/>
-        
+       
+        <Route path="/" element={<Navigate to="/login" replace />} />
+       
         {/* ── Auth Routes ────────────────────────────────────────── */}
         <Route path='/login' element={<LoginPage />} />
         <Route path='/register' element={<RegisterPage />} />
 
+
         {/* ── Mentor ────────────────────────────────────────────────── */}
         {mentorRoute}
+
 
         {/* ── Admin ─────────────────────────────────────────────────── */}
        
         {AdminRoute}
 
+
         {/* ── Student ───────────────────────────────────────────────── */}
         {StudentRoutes}
 
+
       {/* Collge */}
+
 
       {collegeRoute}
 
 
+
+
   {/* Company */}
+
 
       {CompanyRoute}
 
-      
+
+     
         {/* Public certificate verification */}
         <Route path='/verify/:code' element={<VerificationPage />} />
+
 
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </AuthProvider>
   );
+
+
 
 
 }
