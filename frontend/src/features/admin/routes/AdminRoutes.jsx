a33bc7f9b5a208c19aa48ece8b729867926d6a35
Aryan Harke <aryan2005.harke@gmail.com>
2026-06-16 10:53:28 +0530

    Revert "feat: Complete Admin Student Management Module"
    This reverts commit 52220d3deb09fb0fea29d230f760e9ea4648bfa3.

--- frontend/src/features/admin/routes/AdminRoutes.jsx
+++ frontend/src/features/admin/routes/AdminRoutes.jsx
@@ -16,24 +16,24 @@ import PrivateRoute from "../../../routes/PrivateRoute";
 import RoleRoute from "../../../routes/RoleRoute";
 
 const AdminRoute = (
-     <Route element={<PrivateRoute />}>
-     <Route element={<RoleRoute allowedRoles={['admin']} />}>
+  <Route element={<PrivateRoute />}>
+    <Route element={<RoleRoute allowedRoles={["admin"]} />}>
       <Route path="admin" element={<AdminLayout />}>
-          <Route index   path="dashboard"       element={<AdminDashboard />} />
-          <Route path='profile'      element={<AdminProfile />} />
-          <Route path='companies'    element={<AdminCompanies />} />
-          <Route path='colleges'     element={<AdminCollege />} />
-          <Route path='colleges/:id' element={<CollegeDetail />} />
-          <Route path='students'     element={<AdminStudent />} />
-          <Route path='mentors'      element={<AdminMentors />} />
-          <Route path='assesments'   element={<AdminAssesment />} />
-          <Route path='training'     element={<AdminTraining />} />
-          <Route path='drives'       element={<AdminDrives />} />
-          <Route path='notification' element={<AdminNotifications />} />
-          <Route path='disputes'     element={<AdminDisputes />} />
-        </Route>
-        </Route>
-        </Route>
-)
+        <Route index path="dashboard" element={<AdminDashboard />} />
+        <Route path="profile" element={<AdminProfile />} />
+        <Route path="companies" element={<AdminCompanies />} />
+        <Route path="colleges" element={<AdminCollege />} />
+        <Route path="colleges/:id" element={<CollegeDetail />} />
+        <Route path="students" element={<AdminStudent />} />
+        <Route path="mentors" element={<AdminMentors />} />
+        <Route path="assesments" element={<AdminAssesment />} />
+        <Route path="training" element={<AdminTraining />} />
+        <Route path="drives" element={<AdminDrives />} />
+        <Route path="notification" element={<AdminNotifications />} />
+        <Route path="disputes" element={<AdminDisputes />} />
+      </Route>
+    </Route>
+  </Route>
+);
 
 export default AdminRoute;
