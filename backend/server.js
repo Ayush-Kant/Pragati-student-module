51c76566707ed871f79fb87216973f5c1a391142
Soham Deb <sohaam.deb@gmail.com>
2026-06-18 14:03:35 +0530

    feat: backend implementation for Admin Assessment Control (Issue #178)

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import connectDB from "./config/db.js";
 import mentorRoutes from "./routes/mentor.routes.js";
 import adminDashboardRoutes from "./routes/admin.dashboard.routes.js";
 import adminCollegeRoutes from "./routes/admin.college.routes.js";
+import adminAssessmentRoutes from "./routes/admin.assessment.routes.js";
 import companyRoutes from "./routes/company.routes.js";
 import contentRoutes from "./routes/content.routes.js";
 import notificationRoutes from "./routes/notification.routes.js";
@@ -34,6 +35,8 @@ app.use("/api/v1/admin/dashboard", adminDashboardRoutes);
 
 app.use("/api/v1/admin/colleges", adminCollegeRoutes);
 
+app.use("/api/v1/admin/assessments", adminAssessmentRoutes);
+
 app.use("/api/companies", companyRoutes);
 
 app.use("/api/mentor", contentRoutes);
@@ -45,6 +48,8 @@ app.use("/api/auth", authRouter);
 app.use("/api/v1/admin/dashboard", adminDashboardRoutes);
 
 app.use("/api/v1/admin/colleges", adminCollegeRoutes);
+
+app.use("/api/v1/admin/assessments", adminAssessmentRoutes);
 
 app.use("/api/companies", companyRoutes);
 
