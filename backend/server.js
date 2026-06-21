baaf421aff8ad9c15d52f035e7fedef6ce52adf3
mangalarapumaniteja1250-hub <mangalarapumaniteja1250@gmail.com>
2026-06-21 21:28:44 +0530

    feat: add dashboard controllers and routes

--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,29 @@
 import express from "express";
+import dotenv from "dotenv";
 import connectDB from "./config/db.js";
+import dashboardRoutes from "./routes/dashboardRoutes.js";
 
-const PORT = process.env.PORT || 5000;
+dotenv.config();
+
+console.log("POSTGRESQL_URI =", process.env.POSTGRESQL_URI);
 
 const app = express();
+const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 
+app.use("/api/student/dashboard", dashboardRoutes);
+
 app.get("/", (req, res) => {
-    res.json({
-        message: "Backend is running",
-    });
+  res.json({
+    message: "Backend is running",
+  });
 });
 
 app.listen(PORT, () => {
-    console.log(`✅ Server running on PORT : ${PORT}`);
+  console.log(`✅ Server running on PORT : ${PORT}`);
 });
 
-connectDB(process.env.POSTGRESQL_URI).catch((err) => {
-    console.error("❌ PostgreSQL connection failed:", err.message);
+connectDB().catch((err) => {
+  console.error("❌ PostgreSQL connection failed:", err.message);
 });
