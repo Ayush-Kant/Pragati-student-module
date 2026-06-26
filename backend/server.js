02350e3a26536011e8c3b544a354d7c716955aaf
madupojumounika <madupojumounika0@gmail.com>
2026-06-26 15:28:40 +0000

    feat: add admin dispute module (controller, routes, validators)

--- backend/server.js
+++ backend/server.js
@@ -11,6 +11,7 @@ import errorMiddleware from "./middleware/errorMiddleware.js";
 import authRouter from "./routes/auth.routes.js";
 import adminDriveRoutes from "./routes/admin.drive.routes.js";
 import interviewRoutes from "./routes/interview.routes.js";
+import adminDisputeRoutes from "./routes/admin.dispute.routes.js";
 
 import dotenv from "dotenv";
 
@@ -18,9 +19,9 @@ dotenv.config();
 const PORT = process.env.PORT || 5001;
 
 const app = express();
-app.use(errorMiddleware);
-
 app.use(express.json());
+
+app.use(errorMiddleware);
 
 app.use(
   cors({
@@ -42,6 +43,7 @@ app.use("/api/mentor", contentRoutes);
 app.use("/api/v1/company", companyRoutes);
 app.use("/api/v1/company/interviews", interviewRoutes);
 app.use("/api/student/notifications", notificationRoutes);
+app.use("/api/v1/admin/disputes", adminDisputeRoutes);
 
 connectDB(process.env.POSTGRESQL_URI).then(() => {
   app.get("/", (req, res) => {
