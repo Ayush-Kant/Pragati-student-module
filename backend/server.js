a9f9bbb07084f2b893bb2765a030267aae6f6824
Vedanth <vedanth8319@gmail.com>
2026-06-29 19:55:26 +0530

    Updated the mentor.routes.js imported wrong path

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express"
 import connectDB from "./config/db.js";
 import mentorRoutes from "./routes/mentor.routes.js";
+import studentRoutes from './routes/student.routes.js'
 
 const PORT = process.env.PORT || 5000;
 
@@ -8,6 +9,7 @@ const app = express();
 
 
 app.use("/api/mentor", mentorRoutes);
+app.use("/api/students", studentRoutes)
 
 connectDB(process.env.POSTGRESQL_URI).then(() => {
     app.listen(PORT, () => {
