277cc673fb2a764a87eaa2ac2ab593e98de53837
shashank <shashankparutagoudra@gmail.com>
2026-07-02 15:47:31 +0530

    WIP: dispute detail initial setup

---
+++ frontend/src/features/admin/components/DisputeTimeline.jsx
@@ -0,0 +1,7 @@
+export default function DisputeTimeline() {
+  return (
+    <div className="p-4 border rounded-lg">
+      Dispute Timeline
+    </div>
+  );
+}
---
+++ frontend/src/features/admin/pages/DisputeDetail.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function DisputeDetail() {
+    return (
+        <div className="p-6">
+            <h1>Dispute Detail Page</h1>
+        </div>
+    );
+}
