fc45c3f842f95f82bfd29cb38f64163df0706b28
Sreyajoy2004 <sreyajoy24@gmail.com>
2026-07-27 11:13:21 +0530

    chore: add learningModuleDummyData.js file

---
+++ frontend/src/features/student/learning-modules/types/learningModuleDummyData.js
@@ -0,0 +1,10 @@
+/**
+ * Dummy data for learning modules
+ * This data will be used for development before backend integration
+ */
+
+export const dummyModules = [
+  // We'll add modules here
+];
+
+export default dummyModules;
