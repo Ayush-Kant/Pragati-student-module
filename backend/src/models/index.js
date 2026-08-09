1374800c3040c257aa88f2c03d78b98c5d52deb7
rankhambevishu123-stack <rankhambevishu123@gmail.com>
2026-08-09 15:45:54 +0530

    fix(certificates): integrate certificate routes and models into student-team

--- backend/src/models/index.js
+++ backend/src/models/index.js
@@ -3,6 +3,11 @@ import AssessmentQuestion from "./assessmentQuestionModel.js";
 import AssessmentAttempt from "./assessmentAttemptModel.js";
 import AssessmentAnswer from "./assessmentAnswerModel.js";
 import AssessmentResult from "./assessmentResultModel.js";
+import Certificate from './certificateModel.js';
+import Achievement from './achievementModel.js';
+import Badge from './badgeModel.js';
+import StudentCertificate from './studentCertificateModel.js';
+import CertificateVerification from './certificateVerificationModel.js';
 
 Assessment.hasMany(AssessmentQuestion, { foreignKey: "assessmentId", as: "questions" });
 AssessmentQuestion.belongsTo(Assessment, { foreignKey: "assessmentId", as: "assessment" });
@@ -22,4 +27,31 @@ AssessmentResult.belongsTo(AssessmentAttempt, { foreignKey: "attemptId", as: "at
 Assessment.hasMany(AssessmentResult, { foreignKey: "assessmentId", as: "results" });
 AssessmentResult.belongsTo(Assessment, { foreignKey: "assessmentId", as: "assessment" });
 
-export { Assessment, AssessmentQuestion, AssessmentAttempt, AssessmentAnswer, AssessmentResult };
+Certificate.hasMany(StudentCertificate, {
+  foreignKey: 'certificateId',
+  as: 'studentLinks',
+});
+StudentCertificate.belongsTo(Certificate, {
+  foreignKey: 'certificateId',
+  as: 'certificate',
+});
+
+Certificate.hasMany(CertificateVerification, {
+  foreignKey: 'certificateId',
+  as: 'verifications',
+});
+CertificateVerification.belongsTo(Certificate, {
+  foreignKey: 'certificateId',
+  as: 'certificate',
+});
+
+Achievement.hasMany(Certificate, {
+  foreignKey: 'achievementId',
+  as: 'certificates',
+});
+Certificate.belongsTo(Achievement, {
+  foreignKey: 'achievementId',
+  as: 'achievement',
+});
+
+export { Assessment, AssessmentQuestion, AssessmentAttempt, AssessmentAnswer, AssessmentResult, Certificate, Achievement, Badge, StudentCertificate, CertificateVerification };
