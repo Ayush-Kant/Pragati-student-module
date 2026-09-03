6386ff65ea866364f2c86bdfe9fcaab5c095c7ee
Ayush Kant <ayushkant2807@gmail.com>
2026-09-03 14:22:41 +0530

    feat(student-performance): expose PRD performance and submission endpoints

--- backend/controllers/studentPerformance.controller.js
+++ backend/controllers/studentPerformance.controller.js
@@ -2,11 +2,20 @@ import studentPerformanceService from '../services/studentPerformance.service.js
 
 export const getPerformance = async (req, res, next) => {
   try {
-    const data = await studentPerformanceService.getPerformance(req.user);
-    res.json({ success: true, data });
+    const data = await studentPerformanceService.getPerformance(req.user, req.params.driveId || null);
+    return res.status(200).json({ success: true, performance: data, data });
   } catch (error) {
-    next(error);
+    return next(error);
   }
 };
 
-export default { getPerformance };
+export const getSubmissionHistory = async (req, res, next) => {
+  try {
+    const data = await studentPerformanceService.getSubmissionHistory(req.user, req.query);
+    return res.status(200).json({ success: true, ...data });
+  } catch (error) {
+    return next(error);
+  }
+};
+
+export default { getPerformance, getSubmissionHistory };
