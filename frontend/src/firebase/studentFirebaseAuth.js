ff51b749f98db78d8076a26708b9fc8e1096069e
Ayush Kant <ayushkant2807@gmail.com>
2026-09-03 18:25:58 +0530

    feat(student-auth): add Firebase client auth helpers

---
+++ frontend/src/firebase/studentFirebaseAuth.js
@@ -0,0 +1,22 @@
+import {
+  GoogleAuthProvider,
+  getAuth,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+} from "firebase/auth";
+import firebaseApp from "./firebaseConfig";
+
+export const firebaseAuth = getAuth(firebaseApp);
+export const googleProvider = new GoogleAuthProvider();
+googleProvider.setCustomParameters({ prompt: "select_account" });
+
+export const signInStudentWithPassword = (email, password) =>
+  signInWithEmailAndPassword(firebaseAuth, email, password);
+
+export const signInStudentWithGoogle = () =>
+  signInWithPopup(firebaseAuth, googleProvider);
+
+export const getFirebaseIdToken = async () => {
+  if (!firebaseAuth.currentUser) throw new Error("No Firebase user is signed in");
+  return firebaseAuth.currentUser.getIdToken(true);
+};
